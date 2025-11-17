79aed1178ad64717256840dce1c40886edcd2859
Kacper Andrzejewski <andkac@student.agh.edu.pl>
2025-11-17 23:46:41 +0100

    Add project via upload

---
+++ MixedSort/MixedSort/Sort.h.cpp
@@ -0,0 +1,14 @@
+#ifndef _DEBUG_	
+#define _DEBUG_
+
+void InsertSort(int* tab, int n);
+void SelectionSort(int* tab, int n);
+void BubbleSort(int* tab, int n);
+void MixSort(int* tab, int n);
+void HalfSort(int* tab, int n);
+void HeapSort(int* tab, int n);
+//void update( int* tab, int l, int p );
+void QuickSort(int* tab, int n);
+//void quick2( int* tab, int l, int p );
+
+#endif
